c2d5fe7fc6a47ba04aa8a2709cd7816889052bbf
Jerome Chua <jeromechua.jz@gmail.com>
2026-05-31 00:32:12 +0800

    feat: add method to add task
    include method to parse date string

--- internal/service/task_service.go
+++ internal/service/task_service.go
@@ -1,6 +1,10 @@
 package service
 
 import (
+	"fmt"
+	"time"
+	"strings"
+
 	"github.com/jeromechua-12/todo-cli/internal/task"
 )
 
@@ -23,3 +27,45 @@ func NewTaskService(s TaskStorage) *TaskService {
 	return &TaskService{store: s}
 }
 
+func parseDateString(date *string) (*time.Time, error) {
+	if date == nil {
+		return nil, nil
+	}
+
+	// parse date. Only accept yyyy-mm-dd or yyyy-mm-dd hh:mm format
+	*date = strings.TrimSpace(*date)
+	if len(*date) != 10 && len(*date) != 16 {
+		return nil, fmt.Errorf("invalid format for deadline; expected yyyy-mm-dd or yyyy-mm-dd hh:mm")
+	}
+
+	var formattedDeadline *time.Time
+	validDateLayouts := []string{"2006-01-02", "2006-01-02 15:04"}
+	for _, layout := range validDateLayouts {
+		dl, err := time.ParseInLocation(layout, *date, time.Local)
+		if err == nil {
+			// if yyyy-mm-dd, set time as 23:59
+			if len(layout) == 10 {
+				dl = dl.Add(time.Hour * 23).Add(time.Minute * 59)
+			}
+			formattedDeadline = &dl
+			break
+		}
+	}
+	if formattedDeadline == nil {
+		return nil, fmt.Errorf("invalid format for deadline; expected yyyy-mm-dd or yyyy-mm-dd hh:mm")
+	}
+	return formattedDeadline, nil
+}
+
+// adds new task
+func (s *TaskService) AddTask(desc string, deadline *string) error {
+	parsedDeadline, err := parseDateString(deadline)
+	if err != nil {
+		return err
+	}
+	task, err := task.NewTask(desc, parsedDeadline)
+	if err != nil {
+		return err
+	}
+	return s.store.Add(task)
+}
